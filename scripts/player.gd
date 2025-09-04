50923eb650a2d46c63b1613135be53ba949d1ddf
Aaron Lee M. Ybañez <aaronleeybanez@gmail.com>
2025-09-05 00:47:34 +0800

    [Feature][Ybañez] added map tiles, map still on work
    initial

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,24 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+@export var speed: float = 100.0
+@onready var anim = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var input_vector = Vector2.ZERO
+	
+	# Read movement input
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	input_vector = input_vector.normalized()
+	
+	# Movement (no gravity needed for top-down)
+	velocity = input_vector * speed
 	move_and_slide()
+
+	# Animations
+	if input_vector != Vector2.ZERO:
+		anim.play("walk")
+	else:
+		anim.play("idle")
