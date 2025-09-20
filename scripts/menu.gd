4517c0036a9ab457d445ac3e50cc4c3e746ea728
Aaron Lee M. Ybañez <aaronleeybanez@gmail.com>
2025-09-21 02:20:15 +0800

    [Feature][Ybañez] basic menu
    Just added a basic menu, dont have much function yet

---
+++ scripts/menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$VBoxContainer/Start_button.grab_focus()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_option_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_close_button_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_score_button_pressed() -> void:
+	pass # Replace with function body.
