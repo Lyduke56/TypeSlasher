9b9ebfb88c0f01b861921799e081f30d1fc48f4f
Aaron Lee M. Ybañez <aaronleeybanez@gmail.com>
2025-11-19 22:21:38 +0800

    [Feature[Ybanez] Added Fade in for game over
    Introduced a fade-in effect to the game over screen by adding an AnimationPlayer and ColorRect to the scene, along with corresponding animations. Updated the game_over.gd script to trigger the fade-in animation on scene load. Also added a new enemy wave with a werebear to Area 3 boss variant 1.

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -1,10 +1,10 @@
 extends CanvasLayer
-
+@onready var animation_player = $AnimationPlayer
+@onready var color_rect = $AnimationPlayer/ColorRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	animation_player.play("Fade_in")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
